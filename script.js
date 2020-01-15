0a9f7dfcff237d20b7494033bec83f761c3ad77d
kkrociak <59928294+kkrociak@users.noreply.github.com>
2020-01-15 18:15:11 +0100

    update

--- script.js
+++ script.js
@@ -1,47 +1,60 @@
 alert("hello");
 
 
-let map = L.map('mapid');
-map.setView([51.0,24.0],6);
+// let map = L.map('mapid');
+// map.setView([51.0,24.0],6);
 
-let OSMlayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+// let OSMlayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
-let circle = L.circle([50.5, 30.5],
-    {radius: 100},
-    {color:	'#3388ff'}).addTo(map);
+// let circle = L.circle([50.5, 30.5],
+//     {radius: 100},
+//     {color:	'#3388ff'}).addTo(map);
 
-let circleA = L.circle([52.08,23.10],
-    {radius: 100,
-    color:	'#3388ff'}).addTo(map);
-
-
-let circleG = L.circle([54.22,18.38],
-    {radius: 100,
-     color:	'red'}).addTo(map);
+// let circleA = L.circle([52.08,23.10],
+//     {radius: 100,
+//     color:	'#3388ff'}).addTo(map);
 
 
-let circleK = L.circle([50.53,20.37],
-    {radius: 100,
-    color:	'purple'}).addTo(map);
-
-let circleZ = L.circle([51.56,15.30],
-    {radius: 100,
-    color:	'green'}).addTo(map);
+// let circleG = L.circle([54.22,18.38],
+//     {radius: 100,
+//      color:	'red'}).addTo(map);
 
 
-let circleP = L.circle([52.33,19.42],
-    {radius: 100,
-    color:	'gray'}).addTo(map);
+// let circleK = L.circle([50.53,20.37],
+//     {radius: 100,
+//     color:	'purple'}).addTo(map);
 
-L.geoJSON({"type":"FeatureCollection",
-    "features":[{
-        "type":"Feature",
-        "properties":{},
-            "geometry":{
-                "type":"Polygon",
-                "coordinates":[[[-700.0629472732544,50.06019702298089],[-700.0600290298461,50.06019702298089],[-700.0600290298461,50.062648990114795],[-700.0629472732544,50.062648990114795],[-700.0629472732544,50.06019702298089]]]}},
-                {"type":"Feature",
-                "properties":{},
-                "geometry":{"type":"Polygon","coordinates":[[[-700.0619173049927,50.07052749674062],[-700.0547075271605,50.07052749674062],[-700.0547075271605,50.07336454406147],[-700.0619173049927,50.07336454406147],[-700.0619173049927,50.07052749674062]]]}}]});
+// let circleZ = L.circle([51.56,15.30],
+//     {radius: 100,
+//     color:	'green'}).addTo(map);
 
-OSMlayer.addTo(map);
+
+// let circleP = L.circle([52.33,19.42],
+//     {radius: 100,
+//     color:	'gray'}).addTo(map);
+
+// L.geoJSON({"type":"FeatureCollection",
+//     "features":[{
+//         "type":"Feature",
+//         "properties":{},
+//             "geometry":{
+//                 "type":"Polygon",
+//                 "coordinates":[[[-700.0629472732544,50.06019702298089],[-700.0600290298461,50.06019702298089],[-700.0600290298461,50.062648990114795],[-700.0629472732544,50.062648990114795],[-700.0629472732544,50.06019702298089]]]}},
+//                 {"type":"Feature",
+//                 "properties":{},
+//                 "geometry":{"type":"Polygon","coordinates":[[[-700.0619173049927,50.07052749674062],[-700.0547075271605,50.07052749674062],[-700.0547075271605,50.07336454406147],[-700.0619173049927,50.07336454406147],[-700.0619173049927,50.07052749674062]]]}}]});
+
+// OSMlayer.addTo(map);
+
+
+
+require (["esri/Map", "esri/views/MapView"],
+   function(Map, MapView)
+   {
+    let map1 = new Map({basemap:"topo"});
+
+    let mapContainer = new MapView({
+        container: "mapid",  // miejsce gdzie chcemy osadzić mapę (bierzemy klasę z pliku html //
+        map: map1  //czyli nasza zmienna z mapą bazową zdefiniowana wcześniej //
+     }); 
+   })
